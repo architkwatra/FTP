0b90ec3ebda5f1e9da80cc40ba15c24adbec00fe
architkwatra <akwatra@ncsu.edu>
2020-11-02 16:07:03 -0500

    readme

--- Go_Back_N/client.py
+++ Go_Back_N/client.py
@@ -8,118 +8,147 @@ from signal import alarm, signal, setitimer, SIGALRM, ITIMER_REAL
 import pickle
 import collections
 from collections import *
+from constants import *
 
 SENDER_HOST = SENDER_PORT = ""
-max_seq_number = 0
-last_ack_packet = -1
-last_sent_packet = -1
-
-thread_lock = multiprocessing.lock()
-CLIENT_SOCKET = socket(AF_INET, SOCK_DGRAM)
-sliding_window = set()
+maxSequenceNumber = 0
+lastAckPacket = -1
 BUFFER = {}
+slidingWindow = set()
+lastSentPacket = -1
+thread_lock = multiprocessing.Lock()
+CLIENT_SOCKET = socket(AF_INET, SOCK_DGRAM)
+
 sent = False
 timer_start = timer_end = 0
 
-
-def calculate_checksum(chunk):
-    total_checksum = 0
-    chunk = str(chunk)
+def calculate_checksum(data):
+    checksum = 0
     i = 0
-    while i < len(chunk):
-        current_byte = chunk[i]
-        first_byte = ord(chunk[current_byte])
-        second_byte = 0xffff
-        if first_byte+1 < len(chunk):
-            second_byte = ord(chunk[current_byte+1])
-        
-        temp = total_checksum + (first_byte << 8) + second_byte
-        total_checksum = (temp & 0xffff) + (total_checksum >> 16)
-        i += 2
-    return (total_checksum ^ 0xffff)
+    while True:
+        if i >= len(data):
+            break
+        firstByte = ord(data[i])
+        secondByte = 0xffff
+        if i+1 < len(data): 
+            secondByte = ord(data[i+1])
+        temp = checksum + (firstByte << 8) + secondByte
+        checksum = (temp & 0xffff) + (temp >> 16)
+        i+=2
+    return (checksum ^ 0xffff)
 
-def handler():
-    global last_ack_packet, SENDER_HOST, SENDER_PORT
-    n = len(sliding_window)
-    if last_ack_packet == last_sent_packet - n:
-        print("Timeout, sequence number = ", last_ack_packet + 1)
+def setAlarmAndTimer():
+    alarm(0)
+    setitimer(ITIMER_REAL, RTT)
+    
+def handler(timeout_th, frame):
+    global lastAckPacket, SENDER_HOST, SENDER_PORT
+    senderInfo = (SENDER_HOST, SENDER_PORT)
+    n = len(slidingWindow)
+    if lastAckPacket == lastSentPacket - n:
+        print("Timeout enclountered for sequence number = ", lastAckPacket + 1)
         thread_lock.acquire()
-        temp = last_ack_packet+1
-        for i in range(temp, temp + n):
-            alarm(0)
-            setitimer(ITIMER_REAL, RTT)
+        temp = lastAckPacket+1
+        i = temp
+        while i < temp+n:
+        # for i in range(temp, temp + n):
+
+            # alarm(0)
+            # setitimer(ITIMER_REAL, RTT)
+            setAlarmAndTimer()
+
             packet = None
             if i in BUFFER:
                 packet = BUFFER[i]
-                CLIENT_SOCKET.sendto(packet, (SENDER_HOST, SENDER_PORT))
-            thread_lock.release()
+                CLIENT_SOCKET.sendto(packet, senderInfo)
+                
+            i += 1
+        thread_lock.release()
+            
 
-def ack_process(N, SENDER_HOST, SENDER_PORT):
-    global last_ack_packet, last_sent_packet, sent, timer_end, timer_start, sliding_window, BUFFER
+def setSocket():
     ACK_SOCKET = socket(AF_INET, SOCK_DGRAM)
     ACK_SOCKET.bind((ACK_HOST, ACK_PORT))
+    return ACK_SOCKET
+
+
+def deleteAck(lastAckPacket):
+    slidingWindow.remove(lastAckPacket)
+    BUFFER.pop(lastAckPacket)
+
+def ack_process(N, SENDER_HOST, SENDER_PORT):
+    global lastAckPacket, lastSentPacket, sent, timer_end, timer_start, slidingWindow, BUFFER
+    # ACK_SOCKET = socket(AF_INET, SOCK_DGRAM)
+    # ACK_SOCKET.bind((ACK_HOST, ACK_PORT))
+    ACK_SOCKET = setSocket()
 
     while True:
         # recv is a blocking call
         # change 65535 to 4096 for best usage
-        recv_pcket = ACK_SOCKET.loads(65535)
-        reply_type = reply = pickle.loads(recv_pcket)
-        sequence_no, padding, reply_type = reply
-        if reply_type == TYPE_ACK:
+        recievedPacket = ACK_SOCKET.recv(65535)
+        reply = pickle.loads(recievedPacket)
+        # sequenceNumber, padding, reply_type = reply
+        if reply[2] == TYPE_ACK:
             # extracting the last packet seq that was delivered successfully
-            current_ack_seq_number = sequence_no - 1
-            if last_ack_packet > -1:
+            curAckSeqNum = reply[0] - 1
+            if lastAckPacket >= 0:
                 thread_lock.acquire()
             
-            if current_ack_seq_number == max_seq_number:
+            # End of file
+            if curAckSeqNum == maxSequenceNumber:
                 CLIENT_SOCKET.sendto(pickle.dumps(("0", "0", TYPE_EOF, "0")), (SEND_HOST, SEND_PORT))
                 thread_lock.release()
                 sent = True
-                timer_end = datetime.now()
-                total_time = timer_end - timer_start
-                print("total time = ", total_time)
+                # timer_end = datetime.now()
+                # total_time = datetime.now() - timer_start
+                print("total time = ", datetime.now() - timer_start) #   total_time
                 break
             
-            elif current_ack_seq_number > last_ack_packet:
-                while last_ack_packet < current_ack_seq_number:
-                    alarm(0)
-                    setitimer(ITIMER_REAL, RTT)
-                    last_ack_packet += 1
-                    sliding_window.remove(last_ack_packet)
-                    BUFFER.pop(last_ack_packet)
-                    while len(sliding_window) < min(len(BUFFER), N):
-                        if last_sent_packet < max_seq_number:
-                            packet = BUFFER.get(last_sent_packet+1)
+            elif curAckSeqNum > lastAckPacket:
+                while lastAckPacket < curAckSeqNum:
+                    # alarm(0)
+                    # setitimer(ITIMER_REAL, RTT)
+                    setAlarmAndTimer()
+
+                    lastAckPacket += 1
+                    deleteAck(lastAckPacket)
+
+                    while len(slidingWindow) < min(len(BUFFER), N):
+                        if lastSentPacket < maxSequenceNumber:
+                            packet = BUFFER.get(lastSentPacket+1)
                             CLIENT_SOCKET.sendto(packet, (SENDER_HOST, SENDER_PORT))
-                            sliding_window.add(last_sent_packet + 1)
-                            last_sent_packet += 1
+                            slidingWindow.add(lastSentPacket + 1)
+                            lastSentPacket += 1
                 thread_lock.release()
             else:
                 thread_lock.release()
 
             
 def rdt_send(N, SEND_HOST, SEND_PORT):
-    global last_sent_packet, last_ack_packet, sliding_window,client_buffer, t_start
+    global lastSentPacket, lastAckPacket, slidingWindow,client_buffer, t_start
 
     t_start = datetime.now()
     size_client_buffer = len(BUFFER)
 
-    while len(sliding_window) < min(size_client_buffer, N):
-        if last_ack_packet == -1:
-            packet = BUFFER.get(last_sent_packet + 1)
+    while len(slidingWindow) < min(size_client_buffer, N):
+        if lastAckPacket == -1:
+            packet = BUFFER.get(lastSentPacket + 1)
             CLIENT_SOCKET.sendto(packet, (SEND_HOST, SEND_PORT))
-            alarm(0)
-            setitimer(ITIMER_REAL, RTT)
-            last_sent_packet += 1
-            sliding_window.add(last_sent_packet)
+            
+            # alarm(0)
+            # setitimer(ITIMER_REAL, RTT)
+            setAlarmAndTimer()
+
+            lastSentPacket += 1
+            slidingWindow.add(lastSentPacket)
 
 
 
 if __name__ == "__main__":
 
     sequence_number = 0
-
-    if len(sys.arv) != 5:
+    print(sys.argv)
+    if len(sys.argv) < 6:
         print("Please enter the following:")
         print("1. Server IP address 2. Server Port Number 3. File Name 4. Window Size 5. MSS Value")
 
@@ -129,21 +158,22 @@ if __name__ == "__main__":
         FILE_NAME = sys.argv[3] 
         N = int(sys.argv[4]) 
         MSS =  int(sys.argv[5])
-        try:
-            # rb mode means read binary mode 
-            with open(FILE_NAME, 'rb') as f:
-                while True:
-                    chunk = f.read(MSS)
-                    if chunk:
-                        max_seq_number = sequence_number
-                        #get the checksum for the chunk
-                        chunk_checksum = calculate_checksum(chunk)
-                        BUFFER[sequence_number] = pickle.dumps([sequence_number, chunk_checksum, TYPE_DATA, chunk])
-                        sequence_number += 1
-                    break
-        except Exception as e:
-            sys.exit("Please check the below error")
-            print(e)
+        # try:
+        # rb mode means read binary mode 
+        with open(FILE_NAME, 'rb') as f:
+            while True:
+                chunk = f.read(MSS)
+                if chunk:
+                    maxSequenceNumber = sequence_number
+                    #get the checksum for the chunk
+                    chunk_checksum = calculate_checksum(str(chunk))
+                    BUFFER[sequence_number] = pickle.dumps([sequence_number, chunk_checksum, TYPE_DATA, chunk])
+                    sequence_number += 1
+                break
+        # except Exception as e:
+        #     print("XXXXXX", e)
+        #     sys.exit("Please check the below error")
+            
 
         # handler is a custom handler when the signal is recieved
         # The signal.signal() function allows defining custom handlers to 
--- Go_Back_N/server.py
+++ Go_Back_N/server.py
@@ -6,65 +6,90 @@ import os
 from socket import socket, AF_INET, SOCK_DGRAM
 
 
-def compute_checksum_for_chuck(chunk,checksum):
-    for byte in range(0, len(chunk), 2):
-        byte1 = ord(chunk[byte])
-        if byte+1 < len(chunk):
-            byte2 = ord(chunk[byte+1])
-        else:
-            byte2 = 0xffff
-        merged_bytes = (byte1<<8) + byte2
-        checksum_add = checksum + merged_bytes
-        checksum = (checksum_add&0xffff) + (checksum_add>>16)
+# def calculate_checksum(data):
+#     checksum = 0
+#     i = 0
+#     while True:
+#         if i >= len(data):
+#             break
+#         firstByte = ord(data[i])
+#         secondByte = 0xffff
+#         if i+1 < len(data): 
+#             secondByte = ord(data[i+1])
+#         temp = checksum + (firstByte << 8) + secondByte
+#         checksum = (temp & 0xffff) + (temp >> 16)
+#         i+=2
+#     return (checksum ^ 0xffff)
+
+def calculate_checksum(data, checksum):
+    i = 0
+    while True:
+        if i >= len(data):
+            break
+        data = str(data)
+        firstByte = ord(data[i])
+        secondByte = 0xffff
+        if i+1 < len(data): 
+            secondByte = ord(data[i+1])
+        temp = checksum + (firstByte << 8) + secondByte
+        checksum = (temp & 0xffff) + (temp >> 16)
+        i+=2
 
     return (checksum^0xffff)
 
+def setSockt(ackPacket, ackInfo):
+    ACK_SOCKET = socket(AF_INET, SOCK_DGRAM)
+    ACK_SOCKET.sendto(ackPacket, ackInfo)
+    return ACK_SOCKET
 
-def send_acknowledgement(ack_number, ACK_HOST_NAME):
-    ack_packet = pickle.dumps([ack_number, DATA_PAD, TYPE_ACK])
-    ack_socket = socket(AF_INET, SOCK_DGRAM)
-    ack_socket.sendto(ack_packet, (ACK_HOST_NAME, ACK_PORT))
-    ack_socket.close()
+def sendAcknowledgement(seqNumber, ACK_HOST_NAME):    
+    ackInfo = (ACK_HOST_NAME, ACK_PORT)    
+    picketData = [seqNumber, DATA_PAD, TYPE_ACK]
+    ackPacket = pickle.dumps(picketData)
+    ACK_SOCKET = setSockt(ackPacket, ackInfo)
+    ACK_SOCKET.close()
 
 
 def main(PACKET_LOSS_PROB):
-    last_received_packet = -1
-    completed = False
-    while not completed:
-        received_data, addr = server_socket.recvfrom(65535)
+    lastRecPckt = -1
+    done = False
+    while not done:
+        receivedData, addr = SERVER_SOCKET.recvfrom(65535)
         ACK_HOST_NAME = addr[0]
-        received_data = pickle.loads(received_data)
-        packet_sequence_number, packet_checksum, packet_type, packet_data = received_data
-        if packet_type == TYPE_DATA:
+        receivedData = pickle.loads(receivedData)
+        seqNumber, checksum, packetType, packetData = receivedData
+        if packetType == TYPE_DATA:
             if random() >= PACKET_LOSS_PROB:
-                if compute_checksum_for_chuck(packet_data, packet_checksum) != 0:
-                    print("Packet ", packet_sequence_number, " has been dropped due to improper checksum")
+                if calculate_checksum(packetData, checksum) != 0:
+                    print("Packet ", seqNumber, " has been dropped due to improper checksum")
                 else:
-                    if packet_sequence_number == last_received_packet+1:
-                        send_acknowledgement(packet_sequence_number+1, ACK_HOST_NAME)
-                        last_received_packet += 1
+                    if seqNumber == lastRecPckt+1:
+                        sendAcknowledgement(seqNumber+1, ACK_HOST_NAME)
+                        lastRecPckt += 1
+                        # a Opens a file for appending at the end of the file without 
+                        # truncating it. Creates a new file if it does not exist.
 
+                        # b Opens in binary mode.
                         with open(FILE_NAME, 'ab') as file:
-                            file.write(packet_data)
-                    else:
-                        expected_packet = last_received_packet + 1
-                        send_acknowledgement(expected_packet, ACK_HOST_NAME)
+                            file.write(packetData)
+                    else: sendAcknowledgement(lastRecPckt + 1, ACK_HOST_NAME)
             else:
-                print("Packet loss, sequence number = ", packet_sequence_number)
-        elif packet_type == TYPE_EOF:
-            completed = True
-            server_socket.close()
+                print("Packet loss, sequence number = ", seqNumber)
+        elif packetType == TYPE_EOF:
+            done = True
+            SERVER_SOCKET.close()
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 4:
-        print('Need 3 arguments: 1) Server Port Number 2) File Name 3) MSS Value')
+        print("Please enter the following details")
+        print('1. Server Port 2. File Name 3. MSS Value')
     else:
         SERVER_PORT, FILE_NAME, PACKET_LOSS_PROB = int(sys.argv[1]), sys.argv[2], float(sys.argv[3])
-        server_socket = socket(AF_INET, SOCK_DGRAM)
+        SERVER_SOCKET = socket(AF_INET, SOCK_DGRAM)
         HOST_NAME = '0.0.0.0'
 
-        server_socket.bind((HOST_NAME, SERVER_PORT))
+        SERVER_SOCKET.bind((HOST_NAME, SERVER_PORT))
         if os.path.isfile(FILE_NAME):
             os.remove(FILE_NAME)
         main(PACKET_LOSS_PROB)
